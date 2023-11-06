56fa63500f4a4d622fa4fbb4fc3a4daca9e90546
Vũ Huy Hoàng <115575325+HiraDuong@users.noreply.github.com>
2023-11-06 22:53:25 +0700

    Initial commit

---
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.navigation
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.open_button).setOnClickListener{
+            val intent = Intent(this,MainActivity2::class.java);
+            startActivity(intent);
+        }
+    }
+}
